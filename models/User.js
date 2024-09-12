7ec7bf88a0d0a33f1c69174b76382f2f5a2fe3a2
SharpyGH <110199236+Maxeedeeeeee@users.noreply.github.com>
2024-09-11 23:58:57 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    ranks: [String]  // Stores the ranks assigned to the user
+});
+
+module.exports = mongoose.model('User', UserSchema);
