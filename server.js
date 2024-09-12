733dcb5a88b507deabfcb9d1d19bab550c86116f
SharpyGH <110199236+Maxeedeeeeee@users.noreply.github.com>
2024-09-12 00:00:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const User = require('./models/User');
+const Rank = require('./models/Rank');
+const authRoutes = require('./routes/auth');
+const storeRoutes = require('./routes/store');
 
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use('/auth', authRoutes);
+app.use('/store', storeRoutes);
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
