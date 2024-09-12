f060c1b07910f7dd9c65c9d704dca853021023ca
SharpyGH <110199236+Maxeedeeeeee@users.noreply.github.com>
2024-09-11 23:59:56 -0500

    Update Rank.js

--- models/Rank.js
+++ models/Rank.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const RankSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number
+});
+
+module.exports = mongoose.model('Rank', RankSchema);
